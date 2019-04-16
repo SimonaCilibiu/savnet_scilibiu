3186a0491ff2f56785eb69d22720a26cc07e49fc
Simona <Simona@DESKTOP-LOSHAN3.mshome.net>
2019-04-16 21:18:58 +0300

    My first commit

--- src/com/savnet/session03/MyFirstCommit.java
+++ src/com/savnet/session03/MyFirstCommit.java
@@ -3,6 +3,13 @@ package com.savnet.session03;
 public class MyFirstCommit {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+
+		int nota = 6;
+		System.out.println((nota >= 9 && nota <= 10) ? "bine" : //
+				(nota >= 7 && nota <= 8) ? "satisfacator" : //
+						(nota >= 5 && nota <= 6) ? "slab" : //
+								(nota < 5) ? "varza" : "varza");
+
 	}
 
 }
