a25d7b3083afe422c7bb47a490e43d6cb4222c9e
Simona <Simona@DESKTOP-LOSHAN3.mshome.net>
2019-05-21 21:46:12 +0300

    commit

--- src/session10/testfinal/Problema4.java
+++ src/session10/testfinal/Problema4.java
@@ -2,18 +2,21 @@ package session10.testfinal;
 
 public class Problema4 {
 	public static int[] insert(int[] arr, int pos, int n) {
+		int[] result = new int[n];
 		if (pos < 0 || pos > arr.length) {
 			return null;
 		} else {
 			for (int i = 0; i < arr.length; i++) {
 				if (pos == arr.length) {
-					arr[n] = arr[pos - 1];
+					result[n] = arr[pos - 1];
+
 					if (pos < arr.length) {
-						arr[n] = arr[pos];
+						result[n] = arr[pos];
+						return result;
 					}
 				}
 			}
 		}
-		return arr;
+		return result;
 	}
 }
