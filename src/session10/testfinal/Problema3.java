9d81b8df4dae40faa2ebc868f81f029a2646bed8
Simona <Simona@DESKTOP-LOSHAN3.mshome.net>
2019-05-21 21:19:48 +0300

    commit

--- src/session10/testfinal/Problema3.java
+++ src/session10/testfinal/Problema3.java
@@ -4,9 +4,13 @@ public class Problema3 {
 	public static int next(int[] arr, int n) {
 		if (n == arr.length - 1) {
 			return -1;
-		} else if (n > arr.length) {
+		} else {
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] == n) {
+					return arr[i + 1];
+				}
+			}
 			return -2;
 		}
-		return arr[n + 1];
 	}
 }
